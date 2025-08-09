dcc3a5a5159ed2a8439fe359bb4adb4af8332cfe
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2025-08-09 23:59:22 +0900

    배열 삭제 풀이

--- cs/gpt-quiz/2508.js
+++ cs/gpt-quiz/2508.js
@@ -23,3 +23,13 @@ function insertAt(arr, index, value) {
   newArr[index] = value;
   return newArr;
 }
+
+function deleteAt(arr, index) {
+  for (let i = index; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr.length = arr.length - 1; // 마지막 요소 제거
+  return arr;
+}
+
+console.log(deleteAt([1, 2, 3, 4], 2));
